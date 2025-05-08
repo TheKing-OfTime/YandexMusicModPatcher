f0f8a21202266bdce51cae2d66bc88d83ccb45eb
TheKingOfTime <artem.matvienko0@gmail.com>
2025-05-08 23:10:34 +0300

    fix: Set executable name for the application in packager configuration

--- forge.config.js
+++ forge.config.js
@@ -5,6 +5,7 @@ module.exports = {
     packagerConfig: {
         asar: true,
         icon: './assets/icons/icon.ico',
+        executableName: 'YandexMusicModPatcher',
     },
     rebuildConfig: {},
     makers: [
