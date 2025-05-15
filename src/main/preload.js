eb1e26a511a05c747cda93189604d6a553493adb
TheKingOfTime <artem.matvienko0@gmail.com>
2025-05-15 04:35:56 +0300

    fix: Workaround https://github.com/electron/electron/issues/45224

--- src/main/preload.js
+++ src/main/preload.js
@@ -2,13 +2,22 @@ import electron from "electron";
 
 electron.contextBridge.exposeInMainWorld('desktopEvents', {
     send(name, ...args) {
+        console.log('Message sent', name, ...args);
         electron.ipcRenderer.send(name, ...args);
     },
-    on(name, listener) {
+    on(name, callback) {
+
+        const listener = (event, ...args) => {
+            console.log('listener', name, event, ...args);
+            callback(event, ...args);
+        }
+
         electron.ipcRenderer.on(name, listener);
-    },
-    off(name, listener) {
-        electron.ipcRenderer.off(name, listener);
+
+        return () => {
+            console.log('listener off', name, listener);
+            electron.ipcRenderer.off(name, listener);
+        }
     },
     invoke(name, ...args) {
         return electron.ipcRenderer.invoke(name, ...args);
