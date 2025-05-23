4687012bb2576e7285e3e4dfc8955e5d8cfb2a8b
TheKingOfTime <artem.matvienko0@gmail.com>
2025-05-23 21:42:15 +0300

    fix: handleClickOutside now prevents clicks on other elements

--- src/app/components/Dropdown.jsx
+++ src/app/components/Dropdown.jsx
@@ -1,10 +1,12 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import "../styles/Dropdown.css";
 
 export default function Dropdown({ label, description, options, defaultOption, onSelect, disabled = false, className = '', placeholder = 'Выберите...' }) {
     const [isOpen, setIsOpen] = useState(false);
     const [selected, setSelected] = useState(defaultOption);
     const [secondaryDescription, setSecondaryDescription] = useState(defaultOption?.description);
+
+    const dropdownRef = useRef(null);
 
     const handleToggle = () => {
         if (!disabled) {
@@ -17,11 +19,15 @@ export default function Dropdown({ label, description, options, defaultOption, o
         onSelect(option);
         setSecondaryDescription(option.description);
         setIsOpen(false);
+        dropdownRef.current.blur();
     };
 
     const handleClickOutside = useCallback((event) => {
-        if (isOpen && !event.target.closest('.Dropdown')) {
+        if (isOpen && !event.target.closest('.Dropdown_toggle')) {
+            event.stopPropagation();
+            event.preventDefault();
             setIsOpen(false);
+            dropdownRef.current.blur();
         }
     }, [isOpen]);
 
@@ -39,7 +45,7 @@ export default function Dropdown({ label, description, options, defaultOption, o
                     {label && <span className="Dropdown_label">{label}</span>}
                     {(description ?? secondaryDescription) && <span className="Dropdown_description">{description ?? secondaryDescription}</span>}
                 </div>
-                <button className={`Dropdown_toggle${!selected ? ' Dropdown_placeholder' : ''}`} onClick={handleToggle} disabled={disabled}>
+                <button className={`Dropdown_toggle${!selected ? ' Dropdown_placeholder' : ''}`} onClick={handleToggle} disabled={disabled} ref={dropdownRef}>
                     {selected?.label || placeholder}
                     {isOpen && (
                         <ul className="Dropdown_menu">
