f39ca988ac7e88a847e5bdc14c9d5b25fafbcbfc
TheKingOfTime <artem.matvienko0@gmail.com>
2025-06-09 08:24:11 +0300

    chore: Comments clean up

--- src/main/index.js
+++ src/main/index.js
@@ -42,19 +42,15 @@ const createWindow = () => {
         },
     });
 
-    // and load the index.html of the app.
+
     mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
-    // Open the DevTools.
-    //mainWindow.webContents.openDevTools();
     return mainWindow;
 };
 
-const state = initState();
+const state = getState();
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+
 app.whenReady().then(() => {
     const window = createWindow();
 
@@ -64,8 +60,7 @@ app.whenReady().then(() => {
 
     handleApplicationEvents(window);
 
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
+
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
             createWindow();
@@ -73,14 +68,9 @@ app.whenReady().then(() => {
     });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
     }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
