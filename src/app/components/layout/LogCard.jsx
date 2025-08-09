3e598dd475800d145121835dcb6de70beb5d2729
TheKingOfTime <artem.matvienko0@gmail.com>
2025-08-10 02:55:19 +0300

    feat: Add auto scrolling for Log Card

--- src/app/components/layout/LogCard.jsx
+++ src/app/components/layout/LogCard.jsx
@@ -1,9 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useOnLogEntryCreate, useOnPatchProgress } from "../Events.jsx";
 import { LogMessage } from "../ui/LogMessage.jsx";
 
 
 function LogCard({ logEntries, setLogEntries }) {
+
+    const listRef = useRef(null);
+    const [ isAtBottom, setIsAtBottom ] = useState(true);
 
     const addLogEntry = (logEntry) => {
         setLogEntries(prevEntries => [...prevEntries, {
@@ -21,6 +24,12 @@ function LogCard({ logEntries, setLogEntries }) {
     };
 
     useEffect(() => {
+        if (isAtBottom && listRef.current) {
+            listRef.current.scrollTop = listRef.current.scrollHeight - listRef.current.clientHeight;
+        }
+    }, [logEntries, isAtBottom]);
+
+    useEffect(() => {
         const offPatchProgressListener = useOnPatchProgress(handlePatchProgressEvent);
         const offLogEntryCreateEListener = useOnLogEntryCreate(handleLogEntryCreateEvent);
 
@@ -31,14 +40,19 @@ function LogCard({ logEntries, setLogEntries }) {
     }, [])
 
     return (
-    <div className="LogCard scroll_enabled">
-        <ul>{
-            logEntries.map((log, index) => (
+    <div className="LogCard scroll_enabled" ref={listRef}
+         onScroll={() => {
+             const el = listRef.current;
+             setIsAtBottom(el.scrollHeight - el.scrollTop === el.clientHeight);
+         }}
+    >
+        <ul>
+            {logEntries.map((log, index) => (
             <li key={index}>
                 <LogMessage message={log.message} timestamp={log.timestamp}/>
             </li>
-            ))
-        }</ul>
+            ))}
+        </ul>
     </div>
     )
 }
