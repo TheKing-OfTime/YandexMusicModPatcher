3860337a4d0010bac8d8ff9cbdb717996473603a
TheKingOfTime <artem.matvienko0@gmail.com>
2025-09-22 05:51:14 +0300

    feat: Update InlineButton and separate button variants styles to index.css.

--- src/app/components/ui/InlineButton.jsx
+++ src/app/components/ui/InlineButton.jsx
@@ -1,17 +1,27 @@
 import * as React from 'react';
 import '../../styles/InlineButton.css';
 
+import Icon from './Icon.jsx';
 
-function InlineButton({ children, onClick, disabled = false, className = '', type = 'button' }) {
+const variants = {
+    primary: 'PrimaryButton',
+    secondary: 'SecondaryButton',
+    danger: 'DangerButton',
+};
+
+const getVariant = (variant) => variants[variant] ?? variants['primary'];
+
+function InlineButton({ onClick, label=undefined, icon=undefined,  variant = "primary", disabled = false, className = '', type = 'button' }) {
     return (
-    <button
-    type={type}
-    className={`InlineButton ${className}`}
-    onClick={onClick}
-    disabled={disabled}
-    >
-        {children}
-    </button>
+        <button
+            type={ type }
+            className={ `InlineButton ${ getVariant(variant) } ${ className }` }
+            onClick={ onClick }
+            disabled={ disabled }
+        >
+            {icon && <Icon name={icon} size={24} className="InlineButton_icon" />}
+            {label && <span className="InlineButton_label">{label}</span>}
+        </button>
     );
 }
 
