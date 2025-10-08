b9fa6f10e648ff16f78d2c23936520af29f59a17
TheKingOfTime <artem.matvienko0@gmail.com>
2025-10-08 12:56:41 +0300

    feat: Add toast UI

--- src/app/components/layout/TitleBar.jsx
+++ src/app/components/layout/TitleBar.jsx
@@ -1,21 +1,37 @@
-import * as React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import TitleBarButton from '../ui/TitleBarButton.jsx';
 import '../../styles/TitleBar.css';
 import Icon from '../ui/Icon.jsx';
+import { useOnShowToast } from '../Events.jsx';
 
 
 function TitleBar() {
     const platform = window?.CONSTANTS?.PLATFORM ?? 'win32';
-    const macStyle = {
-        justifyContent: 'flex-start',
-        display: 'flex',
-        height: '33px',
-    }
+
+    const [toastLabel, setToastLabel] = useState("Toast!");
+    const [toastShown, setToastShown] = useState(false);
+
+
+    useEffect(() => {
+        const offShowToast = useOnShowToast((event, args) => {
+            setToastLabel(args.label);
+            setToastShown(true);
+
+            setTimeout(() => setToastShown(false), args.duration ?? 3000);
+        })
+
+        return () => {
+            offShowToast()
+        }
+    }, []);
 
     if (platform === 'darwin') {
         return (
-        <header className="TitleBar" style={macStyle}>
-            <p className="TitleBar_app_name"><Icon name="logo" className="TitleBar_app_logo_darwin"/>YandexMusicModPatcher</p>
+        <header className="TitleBar TitleBar_darwin">
+            <div className={ `TitleBar_app_name_container ${toastShown ? 'TitleBar_app_name_container_shifted' : undefined}` }>
+                <p className="TitleBar_toast">{toastLabel}</p>
+                <p className="TitleBar_app_name"><Icon name="logo" className="TitleBar_app_logo_darwin"/>YandexMusicModPatcher</p>
+            </div>
             <div>
             </div>
         </header>
@@ -25,7 +41,10 @@ function TitleBar() {
     return (
     <header className="TitleBar">
         <Icon name="logo" className="TitleBar_app_logo"/>
-        <p className="TitleBar_app_name">YandexMusicModPatcher</p>
+        <div className={ `TitleBar_app_name_container ${toastShown ? 'TitleBar_app_name_container_shifted' : undefined}` }>
+            <p className="TitleBar_toast">{toastLabel}</p>
+            <p className="TitleBar_app_name">YandexMusicModPatcher</p>
+        </div>
         <div>
             <TitleBarButton variant="minimize"/>
             <TitleBarButton variant="maximize"/>
